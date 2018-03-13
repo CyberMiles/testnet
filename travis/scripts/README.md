56adffddbd5cb56184cc3b4ae4cbcc9876a0d1af
maggie-5miles <maggie@wespoke.com>
2018-03-13 18:11:10 +0800

    travis-1-dev

---
+++ travis/scripts/README.md
@@ -0,0 +1,72 @@
+# How to use
+
+It's assumed that you have already [setup docker](https://docs.docker.com/install/) on your computer.
+
+Configuration and data will be stored at `/travis` in the container. This directory will also be exposed as a volume. The ports `8545`, `46656` and `46657` will be exposed to connect.
+
+## Create and start a local node
+
+```sh
+# prepare volume folder
+mkdir -p ~/volumes/local
+
+# initialize
+docker run --rm -v ~/volumes/local:/travis ywonline/travis node init --home=/travis
+
+# start
+docker run --rm -v ~/volumes/local:/travis -p 46657:46657 -p 8545:8545 ywonline/travis node start --home=/travis
+```
+
+The chain's status is at the /status end-point:
+
+```sh
+curl http://localhost:46657/status
+```
+
+## Setup a testnet
+
+### Initialize
+
+Copy all files required to setup a testnet from repository. The default testnet is a 6 node cluster, with 4 validators in it.
+
+```sh
+mkdir -p ~/volumes
+git clone https://github.com/CyberMiles/testnet.git ~/volumes/testnet
+```
+
+Or run `scripts/cluster.sh [count_of_all_nodes] [count_of_validators]` to setup a cluster differently.
+
+### Start up
+
+```sh
+cd ~/volumes/testnet/travis/scripts
+
+# start with 4 nodes which are all validators
+docker-compose up -d validators
+# then start 2 normal nodes and join the chain
+docker-compose up -d normal
+
+# or start all 6 nodes together
+docker-compose up -d all
+```
+
+### Status and logs
+
+```sh
+# check status of these containers
+docker-compose ps
+
+# check logs
+docker-compose logs -f
+
+# specify a node to check
+docker-compose logs -f -t node-1
+
+# chain's status is at the `/status` end-point
+curl http://localhost:46657/status
+```
+
+### Stops containers and cleanup
+```sh
+docker-compose down
+```
---
+++ travis/scripts/cluster.sh
@@ -0,0 +1,74 @@
+#!/bin/bash
+if [ "$1" == "-h" ]; then
+  echo "Usage: `basename $0` [inst_count] [validator_count]"
+  exit
+fi
+
+INST_COUNT=$1
+VALIDATOR_COUNT=$2
+
+cd "$(dirname "$0")"
+BASE_DIR=$(dirname $PWD)/nodes
+
+TRPCPORT=46657
+TP2PPORT=46656
+ERPCPORT=8545
+
+# init params
+if [[ (-z $INST_COUNT) || (! $INST_COUNT =~ ^[0-9]+$) ]]; then
+  INST_COUNT=1
+  VALIDATOR_COUNT=1
+elif [[ ! $VALIDATOR_COUNT =~ ^[0-9]+$ ]]; then
+  VALIDATOR_COUNT=$INST_COUNT
+else
+  if [[ $INST_COUNT -le 0 ]]; then
+    echo "wrong inst_count"
+    exit
+  fi
+  if [[ $INST_COUNT -lt $VALIDATOR_COUNT ]]; then
+    VALIDATOR_COUNT=$INST_COUNT
+  fi
+fi
+
+if [ $INST_COUNT -eq 0 ]; then
+  exit
+fi
+
+echo "inst_count: $INST_COUNT, validator_count: $VALIDATOR_COUNT"
+read -p "Press enter to continue..."
+
+# init & config.toml
+for i in `seq 1 $INST_COUNT`
+do
+  dir=$BASE_DIR/node$i
+
+  # make node* directory if not exist
+  mkdir -p $dir
+
+  cd $dir
+
+  rm -rf *
+  travis node init --home .
+  if [ $i -ne 1 ]; then
+    seeds=`for ((i=1;i<=$VALIDATOR_COUNT;i++)) do echo node-$i:$TP2PPORT; done | sed '$!s/$/,/' | tr -d '\n'`
+  else
+    # seeds is empty string for the first instance
+    seeds=""
+  fi
+  sed -i '' "s/seeds = \"\"/seeds = \"$seeds\"/g" ./config.toml
+  sed -i '' "s/moniker = .*$/moniker = \"node-$i\"/g" ./config.toml
+done
+
+# genesis.json
+cd $BASE_DIR
+
+# combine the public keys of all validators, and set to genesis.json
+validators=`for ((i=1;i<=$VALIDATOR_COUNT;i++)) do echo node$i/genesis.json; done \
+  | xargs jq -r '.validators[0]' | sed '$!s/^}$/},/' |tr -d '\n'`
+sed -i '' "s/\"validators\":\[.*\]/\"validators\":\[$validators\]/" node1/genesis.json
+# set genesis_time, chain_id and validator.power
+jq '(.genesis_time) |= "2018-03-15T00:00:00Z" | (.chain_id) |= "travis-1-dev" | (.validators[]|.power) |= 1000' \
+  node1/genesis.json > tmp && mv tmp node1/genesis.json
+
+# copy from node1 to other nodes
+for ((i=2;i<=$INST_COUNT;i++)) do echo node$i/genesis.json; done | xargs -n 1 cp node1/genesis.json

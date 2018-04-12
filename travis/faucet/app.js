d9e2c5cdbb67783cb35edd7b04c06d2f5552c2d2
wangshishuo <wangshishuo@gmail.com>
2018-04-12 17:13:40 +0800

    faucet

--- travis/faucet/app.js
+++ travis/faucet/app.js
@@ -1,11 +1,12 @@
 const Web3 = require('web3')
 const express = require('express')
-const bodyParser = require('body-parser');
+const bodyParser = require('body-parser')
+const request = require('request')
 
-// const BaseAddr = '0x77beb894fc9b0ed41231e51f128a347043960a9d'
-const BaseAddr = '0xcc4e6cd091c7d9157e029a968990099a8e494b92'
-// const BasePwd = 't0B*%829z$Zp'
-const BasePwd = '1234'
+const BaseAddr = '0x7eff122b94897ea5b0e2a9abf47b86337fafebdc'
+const BasePwd = 'basepwd'
+
+const TESTabi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
 
 const app = express()
 
@@ -15,28 +16,78 @@ app.use(bodyParser.json());
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
-  // set the provider you want from Web3.providers
   web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
 }
+var testTokenInstance = web3.eth.contract(TESTabi).at('0xb6b29ef90120bec597939e0eda6b8a9164f75deb')
 
 app.post('/send', (req, res) => {
+  // verify the recaptcha
+  if (!req.body.recaptcha) {
+    res.json({"error": "Not verified Recaptcha"})
+    return
+  }
+
+  request.post(
+    {
+      url: 'https://www.google.com/recaptcha/api/siteverify',
+      form: {
+        secret: 'recaptcha-secret',
+        response: req.body.recaptcha
+      }
+    },
+    function(error, response, body) {
+      if (error) {
+        if (error.errno) {
+          res.json({"error": error.errno})
+        } else {
+          res.json({"error": error})
+        }
+        return
+      }
+      if (!response || response.statusCode != 200) {
+        res.json({"error": "Can't verify Recaptcha"})
+        return
+      }
+
+      try {
+        body = JSON.parse(body)
+      } catch (e) {
+      }
+
+      if (body.success) {
+        sendCmt(req.body.to, res)
+      } else {
+        console.log(body)
+        res.json({"error": "Failed verify recaptcha"})
+      }
+    }
+  )
+
+})
+
+function sendCmt(to, res) {
   web3.personal.unlockAccount(BaseAddr, BasePwd)
   web3.eth.sendTransaction(
     {
       from: BaseAddr,
-      to: req.body.to,
+      to: to,
       value: 1000
     },
-    function(err, txHash) {
-      web3.personal.lockAccount(BaseAddr)
+    function(err, cmtHash) {
       if (err) {
+        web3.personal.lockAccount(BaseAddr)
         console.error(err)
         res.json({"error": err})
         return
       }
-      res.json({"hash": txHash})
+
+      // sendTEST
+      testTokenInstance.transfer.sendTransaction(to, 1000, {from: BaseAddr}, function(err, testHash) {
+        web3.personal.lockAccount(BaseAddr)
+        res.json({"cmtHash": cmtHash, "testHash": testHash})
+      })
     }
   )
-})
+}
 
 app.listen(3000, () => console.log('App listening on port 3000!'))

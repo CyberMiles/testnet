934419017475ee18dd3dc33a28c26c43b518afa8
wangshishuo <wangshishuo@gmail.com>
2018-06-14 12:25:46 +0800

    use environment variables; summarize validators info

--- travis/faucet/app.js
+++ travis/faucet/app.js
@@ -4,10 +4,11 @@ const bodyParser = require('body-parser')
 const request = require('request')
 const Geetest = require('gt3-sdk')
 const geoip   = require('geoip-lite')
+const validators = require('./validators')
 
 const DefaultAddrs = [
   '18.220.151.169',
-  '18.218.23.115',
+  '18.188.237.161',
   '52.15.137.235',
   '18.222.2.50',
   '18.188.21.0',
@@ -16,12 +17,12 @@ const DefaultAddrs = [
 ]
 
 let geetest = new Geetest({
-  geetest_id: 'geetest_id',
-  geetest_key: 'geetest_key'
+  geetest_id: process.env['TRAVIS_FAUCET_GEETEST_ID'],
+  geetest_key: process.env['TRAVIS_FAUCET_GEETEST_KEY']
 })
 
 const BaseAddr = '0x7eff122b94897ea5b0e2a9abf47b86337fafebdc'
-const BasePwd = 'basepwd'
+const BasePwd = process.env['TRAVIS_FAUCET_COINBASE_PWD']
 
 const TESTabi = [{"constant":true,"inputs":[],"name":"name","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_value","type":"uint256"}],"name":"approve","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"totalSupply","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_from","type":"address"},{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transferFrom","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"INITIAL_SUPPLY","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"decimals","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"unpause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"paused","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_subtractedValue","type":"uint256"}],"name":"decreaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"}],"name":"balanceOf","outputs":[{"name":"balance","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[],"name":"pause","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[],"name":"owner","outputs":[{"name":"","type":"address"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":true,"inputs":[],"name":"symbol","outputs":[{"name":"","type":"string"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"_to","type":"address"},{"name":"_value","type":"uint256"}],"name":"transfer","outputs":[{"name":"","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":false,"inputs":[{"name":"_spender","type":"address"},{"name":"_addedValue","type":"uint256"}],"name":"increaseApproval","outputs":[{"name":"success","type":"bool"}],"payable":false,"stateMutability":"nonpayable","type":"function"},{"constant":true,"inputs":[{"name":"_owner","type":"address"},{"name":"_spender","type":"address"}],"name":"allowance","outputs":[{"name":"","type":"uint256"}],"payable":false,"stateMutability":"view","type":"function"},{"constant":false,"inputs":[{"name":"newOwner","type":"address"}],"name":"transferOwnership","outputs":[],"payable":false,"stateMutability":"nonpayable","type":"function"},{"inputs":[],"payable":false,"stateMutability":"nonpayable","type":"constructor"},{"anonymous":false,"inputs":[],"name":"Pause","type":"event"},{"anonymous":false,"inputs":[],"name":"Unpause","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"previousOwner","type":"address"},{"indexed":true,"name":"newOwner","type":"address"}],"name":"OwnershipTransferred","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"owner","type":"address"},{"indexed":true,"name":"spender","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Approval","type":"event"},{"anonymous":false,"inputs":[{"indexed":true,"name":"from","type":"address"},{"indexed":true,"name":"to","type":"address"},{"indexed":false,"name":"value","type":"uint256"}],"name":"Transfer","type":"event"}]
 
@@ -34,7 +35,7 @@ app.use(bodyParser.json());
 if (typeof web3 !== 'undefined') {
   web3 = new Web3(web3.currentProvider);
 } else {
-  web3 = new Web3(new Web3.providers.HttpProvider("http://localhost:8545"));
+  web3 = new Web3(new Web3.providers.HttpProvider('http://' + process.env['TRAVIS_FAUCET_RPC_HOST'] + ':' + process.env['TRAVIS_FAUCET_RPC_PORT']));
 }
 var testTokenInstance = web3.eth.contract(TESTabi).at('0xb6b29ef90120bec597939e0eda6b8a9164f75deb')
 
@@ -58,7 +59,7 @@ app.get('/cn/init', (req, res) => {
 app.post('/nodes', (req, res) => {
   request.get(
     {
-      url: 'http://travis-node.cybermiles.io:46657/net_info'
+      url: process.env['TRAVIS_FAUCET_TM_URL'] + '/net_info'
     },
     (error, response, body) => {
       if (error) {
@@ -113,7 +114,7 @@ app.post('/send', (req, res) => {
     {
       url: 'https://www.google.com/recaptcha/api/siteverify',
       form: {
-        secret: 'recaptcha-secret',
+        secret: process.env['TRAVIS_FAUCET_GOOGLE_RECAPTCHA_SECRET'],
         response: req.body.recaptcha
       }
     },

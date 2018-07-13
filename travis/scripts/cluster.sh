55710898575cd1c2bfeb6aaef3fe6cac148429d9
maggie-5miles <maggie@wespoke.com>
2018-07-13 17:42:05 +0800

    set initial comp_rate to 0.2

--- travis/scripts/cluster.sh
+++ travis/scripts/cluster.sh
@@ -108,7 +108,7 @@ rm validators.json
 
 # set genesis_time, chain_id and validator.power
 jq --arg CHAIN_DATE $CHAIN_DATE --arg CHAIN_ID $CHAIN_ID \
-  '(.genesis_time) |= $CHAIN_DATE | (.chain_id) |= $CHAIN_ID | (.validators[]|.power) |= "10000"' \
+  '(.genesis_time) |= $CHAIN_DATE | (.chain_id) |= $CHAIN_ID | (.validators[]|.power) |= "10000" | (.validators[]|.comp_rate) |= "0.2"' \
   node1/config/genesis.json > tmp && mv tmp node1/config/genesis.json
 # set address
 for ((i=1;i<$VALIDATOR_COUNT;i++)) do
